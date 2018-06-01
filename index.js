3512c0d9d7c19672891420fc7dd00a26ee1b486a
Julian Bonomini <julian.bonomini@gmail.com>
2018-06-01 18:00:11 -0300

    code beautify

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Dimensions = require('react-native')
 
 const wp = percent => {
-  const width = Dimensions.get('window').width
+  const { width } = Dimensions.get('window')
   return (percent * width) / 100
 }
 
 const hp = percent => {
-  const height = Dimensions.get('window').height
+  const { height } = Dimensions.get('window')
   return (percent * height) / 100
 }
 
